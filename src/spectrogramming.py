d10494f552fa1f271320ac8988e34de9369e8d61
Nicholas Jourjine <jourjine.nicholas@gmail.com>
2023-01-05 07:57:55 -0500

    documented which functions are used by Segmenting and UMAP

--- src/spectrogramming.py
+++ src/spectrogramming.py
@@ -16,70 +16,8 @@ import time
 from sklearn.preprocessing import StandardScaler
 import umap
 from scipy.signal import butter, lfilter
-#modified from https://github.com/timsainb/avgn_paper/blob/V2/avgn/signalprocessing/create_spectrogram_dataset.py
-def log_resize_spec(spec, scaling_factor):
 
-    resize_shape = [int(np.log(np.shape(spec)[1]) * scaling_factor), np.shape(spec)[0]]
-    spec = (spec * 255).astype(np.float32) #make data type and range compatible with Image
-    resize_spec = np.array(Image.fromarray(spec).resize(resize_shape))
-    return resize_spec
-
-def pad_spectrogram(spectrogram, pad_length):
-    """ Pads a spectrogram to being a certain length
-    """
-    excess_needed = pad_length - np.shape(spectrogram)[1]
-    pad_left = np.floor(float(excess_needed) / 2).astype("int")
-    pad_right = np.ceil(float(excess_needed) / 2).astype("int")
-    return np.pad(
-        spectrogram, [(0, 0), (pad_left, pad_right)], "constant", constant_values=0
-    )
-
-
-
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    #functions to bandpass the audio 
-    #modified from https://timsainburg.com/python-mel-compression-inversion.html#python-mel-compression-inversion
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype="band")
-    return b, a
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    #functions to bandpass the audio 
-    #modified from https://timsainburg.com/python-mel-compression-inversion.html#python-mel-compression-inversion
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
-#function to generate spectrograms
-def scipy_specgram(data, 
-                   fs, 
-                   nperseg, 
-                   noverlap, 
-                   thresh, 
-                   scaling_factor = None):
-    
-	#get the spectrogram
-	f,t,specgram = stft(data, fs, nperseg=nperseg, noverlap=noverlap) #default winow is Hann
-
-	#make it pretty
-	specgram = np.abs(specgram) #remove complex
-	specgram = np.log(specgram)  # take log
-
-	if scaling_factor!= None:
-		specgram = log_resize_spec(specgram, scaling_factor= scaling_factor)    
-		specgram[specgram < thresh] = thresh
-		specgram = (specgram - np.min(specgram)) / (np.max(specgram) - np.min(specgram))
-
-	else: 
-		specgram[specgram < thresh] = thresh
-		specgram = (specgram - np.min(specgram)) / (np.max(specgram) - np.min(specgram))
-		
-	return f,t,specgram
-
-#function to generate spectrogram using interpolation a la Goffinet 2021	
+#definitely keep - documented now on evernote 20230105
 def scipy_specgram_interpolate(data, 
                           fs, 
                           nperseg, 
@@ -114,10 +52,83 @@ def scipy_specgram_interpolate(data,
 
 	return f,t,specgram
 
-#make spectrograms from wav clips in the directory clips_dir using parameters in the spec_params dictionary
-#then pad them to the length of the longest clip in clips_dir
-#returns a list of padded spectrograms
-#
+
+def wavs_to_umap(segmenting_option, clips_dir, noise_floors_path, species, noise_floor, spec_params, num_to_process, filtered_clips, interpolate, version, save_root):
+#go throught the full pipeline from spectrogram generation to umap plotting
+	
+		
+	#get spectrograms
+	specs_list, source_files = specs_from_wavs(clips_dir = clips_dir, 
+										  noise_floors_path=noise_floors_path, 
+										  species = species,
+										  noise_floor = noise_floor,
+										  spec_params=spec_params, 
+										  num_to_process = num_to_process, 
+										  filtered_clips = filtered_clips, 
+										  interpolate=interpolate)
+										  
+	#linearize
+	specs_lin, shape = linearize_specs(specs_list)
+	del specs_list #free up space
+	print(shape)
+	
+	#zscore
+	df_umap, zscored = zscore_specs(specs_lin, source_files)
+	del specs_lin #free up space
+	
+	#embed
+	umap1, umap2 = get_umap(zscored)
+	del zscored #free up space
+	
+	#plot 
+	print('plotting...')
+	fig = plt.figure(figsize=(10,10))
+	ax = fig.add_subplot(1,1,1)
+	ax = plt.scatter(
+		umap1,
+		umap2,
+		c = 'k',
+		s = 1,
+		alpha = .25, 
+		cmap='viridis')
+	plt.xlim([-20,20])
+	plt.ylim([-20,20])
+	plt.show
+	plots_save_dir = save_root+'umap_plots/'
+	if 'umap_plots' not in os.listdir(save_root):
+		os.mkdir(plots_save_dir)
+	plot_save_name = spec_params['species'] + '_black_' + version + '.jpeg'
+	plt.savefig(plots_save_dir+plot_save_name, dpi=600)
+	
+	#save
+	print('saving umap coordinates...')
+	file_format = '.feather'
+	df_umap = df_umap.reset_index(drop=True)
+	coordinates_save_dir = save_root+'umap_coordinates/'
+	if 'umap_coordinates' not in os.listdir(save_root):
+		os.mkdir(coordinates_save_dir)
+	coordinates_save_name = spec_params['species'] + version + file_format
+	
+	save_name = coordinates_save_dir+coordinates_save_name
+	df_umap.columns = df_umap.columns.map(str)
+	df_umap['umap1'] = umap1
+	df_umap['umap2'] = umap2
+	df_umap.to_feather(save_name)
+	
+	#TODO: get this to work
+	# if coordinates_save_name not in coordinates_save_dir:
+# 		save_umap(df_umap, umap1, umap2, save_name = save_name, file_format = file_format)
+# 	else:
+# 		print('already processed...')
+
+
+
+#modified from https://github.com/timsainb/avgn_paper/blob/V2/avgn/signalprocessing/create_spectrogram_dataset.py
+
+
+
+
+
 def specs_from_wavs(clips_dir, species, filtered_clips, noise_floors_path, noise_floor, spec_params, num_to_process, include_scratch = True, interpolate=False):
 
 #TODO
@@ -224,7 +235,191 @@ def specs_from_wavs(clips_dir, species, filtered_clips, noise_floors_path, noise
     print('done.')
     return specs_list, source_files
 
+
+
 #make spectrograms from a segments dataframe without saving wav clips first - useful for trouble shooting (faster)
+
+
+
+
+
+
+
+
+
+def linearize_specs(specs_list):
+	
+	#print some useful info
+	specs = np.array(specs_list)
+	shape = np.shape(specs)
+	print('shape of spectrograms array is:', np.shape(specs))
+	
+	#linearize
+	print('linearizing spectrograms...')
+	num_features = specs.shape[-1]*specs.shape[-2]
+	specs_lin = specs.reshape([-1, num_features])
+	print('done.')
+	return specs_lin, shape
+
+#z score linearized spectrograms and return a dataframe of the zscored specs and original specs
+def zscore_specs(specs_lin, source_files):
+	#make a dataframe
+	df_umap = pd.DataFrame(specs_lin)
+	df_umap['source_file'] = source_files
+	
+	# Z-score the spectrogams
+	print('z scoring...')
+	zscored = StandardScaler().fit_transform(df_umap.drop(columns=['source_file']))	
+	print('done.')
+	return df_umap, zscored
+
+#take a data frame of zscored spectrograms (no source_file information) and find a umap embedding
+#return umap 1 and umap 2 coordinates as separate arrays
+def get_umap(standardized_features):
+	
+	#find an embedding
+	print('finding a umap embedding...')
+	
+	start_time = time.time()
+	reducer = umap.UMAP()
+	embedding = reducer.fit_transform(standardized_features)
+	print('embedding took', time.time() - start_time, 'seconds.')
+	
+	umap1 = embedding[:, 0]
+	umap2 = embedding[:, 1]
+
+	return umap1, umap2
+
+#save a dataframe of all spectrograms and their umap coordinates
+
+
+
+
+
+
+#probably keep and make cells to use them in Segmenting and UMAP.ipynb
+def save_umap(df_umap, umap1, umap2, save_name = None, file_format = 'feather'):
+	
+	if file_format == 'csv':
+		df_umap['embedding_dim_1'] = umap1
+		df_umap['embedding_dim_2'] = umap2
+		df_umap.to_csv(save_name, index=False)
+		print('done.')
+	
+	elif file_format == 'feather':
+		df_umap['embedding_dim_1'] = umap1
+		df_umap['embedding_dim_2'] = umap2
+		df_umap.to_feather(save_name)
+		print('done.')
+
+		
+# make an aggregate spectrogram from  list of source files (wav clips)
+
+def spec_avg_from_list(spec_list):
+    #get average
+    avg_spec_image = np.mean(spec_list, axis=0)
+        
+    return avg_spec_image
+     
+    
+def show_specs(frame, num_freq_bins, num_time_bins, columns_to_drop):
+	for i in range(len(frame)):
+		print(frame['source_file'].iloc[i])
+		to_plot = frame.drop(columns = columns_to_drop)
+		img = to_plot.iloc[i]
+		img = np.array(img).reshape((num_freq_bins, num_time_bins))
+		plt.imshow(img, origin = 'lower', extent = (num_freq_bins, 0, num_time_bins, 0 ))
+		plt.show()
+
+def files_from_umap(frame, umap1_name, umap2_name, umap1_thresh, umap2_thresh, source_dir):
+
+    #get the spectrograms
+    temp = frame[(frame[umap1_name] > umap1_thresh[0]) & (frame[umap1_name] < umap1_thresh[1]) & (frame[umap2_name] > umap2_thresh[0]) & (frame[umap2_name] < umap2_thresh[1])]
+    source_files = [source_dir+i for i in temp['source_file']]
+    return source_files
+
+def spec_avg_from_umap(frame, umap1_name, umap2_name, umap1_thresh, umap2_thresh, num_freq_bins, num_time_bins):
+
+    #get the spectrograms
+    temp = frame[(frame[umap1_name] > umap1_thresh[0]) & (frame[umap1_name] < umap1_thresh[1]) & (frame[umap2_name] > umap2_thresh[0]) & (frame[umap2_name] < umap2_thresh[1])]
+    
+    if 'label' in temp.columns:
+        avg_spec_raw = np.array(temp.drop(columns = ['label', 'source_file', umap1_name, umap2_name]))
+    else:
+        avg_spec_raw = np.array(temp.drop(columns = ['source_file', umap1_name, umap2_name]))
+    
+    #get the average
+    avg_spec_image = np.mean(avg_spec_raw, axis=0).reshape((num_freq_bins, num_time_bins))
+    
+    #return the individual spectrograms
+    return temp, avg_spec_image
+#TODO - properly comment this. Looks like segmenting_option doesn't do anything
+
+	
+#probably remove   
+def log_resize_spec(spec, scaling_factor):
+
+    resize_shape = [int(np.log(np.shape(spec)[1]) * scaling_factor), np.shape(spec)[0]]
+    spec = (spec * 255).astype(np.float32) #make data type and range compatible with Image
+    resize_spec = np.array(Image.fromarray(spec).resize(resize_shape))
+    return resize_spec
+
+def pad_spectrogram(spectrogram, pad_length):
+    """ Pads a spectrogram to being a certain length
+    """
+    excess_needed = pad_length - np.shape(spectrogram)[1]
+    pad_left = np.floor(float(excess_needed) / 2).astype("int")
+    pad_right = np.ceil(float(excess_needed) / 2).astype("int")
+    return np.pad(
+        spectrogram, [(0, 0), (pad_left, pad_right)], "constant", constant_values=0
+    )
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    #functions to bandpass the audio 
+    #modified from https://timsainburg.com/python-mel-compression-inversion.html#python-mel-compression-inversion
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype="band")
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    #functions to bandpass the audio 
+    #modified from https://timsainburg.com/python-mel-compression-inversion.html#python-mel-compression-inversion
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+#function to generate spectrograms
+def scipy_specgram(data, 
+                   fs, 
+                   nperseg, 
+                   noverlap, 
+                   thresh, 
+                   scaling_factor = None):
+    
+	#get the spectrogram
+	f,t,specgram = stft(data, fs, nperseg=nperseg, noverlap=noverlap) #default winow is Hann
+
+	#make it pretty
+	specgram = np.abs(specgram) #remove complex
+	specgram = np.log(specgram)  # take log
+
+	if scaling_factor!= None:
+		specgram = log_resize_spec(specgram, scaling_factor= scaling_factor)    
+		specgram[specgram < thresh] = thresh
+		specgram = (specgram - np.min(specgram)) / (np.max(specgram) - np.min(specgram))
+
+	else: 
+		specgram[specgram < thresh] = thresh
+		specgram = (specgram - np.min(specgram)) / (np.max(specgram) - np.min(specgram))
+		
+	return f,t,specgram
+
+#function to generate spectrogram using interpolation a la Goffinet 2021	
+
+
+
 def specs_from_segs(seg_df_path,raw_wavs_dir, spec_params, num_to_process, species=None, units= 's', interpolate=False):
 
 	#list to add the spectrograms to
@@ -312,174 +507,6 @@ def specs_from_segs(seg_df_path,raw_wavs_dir, spec_params, num_to_process, speci
 	return specs_list, source_files
 
 #linearize spectrograms for umap
-def linearize_specs(specs_list):
-	
-	#print some useful info
-	specs = np.array(specs_list)
-	shape = np.shape(specs)
-	print('shape of spectrograms array is:', np.shape(specs))
-	
-	#linearize
-	print('linearizing spectrograms...')
-	num_features = specs.shape[-1]*specs.shape[-2]
-	specs_lin = specs.reshape([-1, num_features])
-	print('done.')
-	return specs_lin, shape
-
-#z score linearized spectrograms and return a dataframe of the zscored specs and original specs
-def zscore_specs(specs_lin, source_files):
-	#make a dataframe
-	df_umap = pd.DataFrame(specs_lin)
-	df_umap['source_file'] = source_files
-	
-	# Z-score the spectrogams
-	print('z scoring...')
-	zscored = StandardScaler().fit_transform(df_umap.drop(columns=['source_file']))	
-	print('done.')
-	return df_umap, zscored
-
-#take a data frame of zscored spectrograms (no source_file information) and find a umap embedding
-#return umap 1 and umap 2 coordinates as separate arrays
-def get_umap(standardized_features):
-	
-	#find an embedding
-	print('finding a umap embedding...')
-	
-	start_time = time.time()
-	reducer = umap.UMAP()
-	embedding = reducer.fit_transform(standardized_features)
-	print('embedding took', time.time() - start_time, 'seconds.')
-	
-	umap1 = embedding[:, 0]
-	umap2 = embedding[:, 1]
-
-	return umap1, umap2
-
-#save a dataframe of all spectrograms and their umap coordinates
-def save_umap(df_umap, umap1, umap2, save_name = None, file_format = 'feather'):
-	
-	if file_format == 'csv':
-		df_umap['embedding_dim_1'] = umap1
-		df_umap['embedding_dim_2'] = umap2
-		df_umap.to_csv(save_name, index=False)
-		print('done.')
-	
-	elif file_format == 'feather':
-		df_umap['embedding_dim_1'] = umap1
-		df_umap['embedding_dim_2'] = umap2
-		df_umap.to_feather(save_name)
-		print('done.')
-
-		
-# make an aggregate spectrogram from  list of source files (wav clips)
-
-def spec_avg_from_list(spec_list):
-    #get average
-    avg_spec_image = np.mean(spec_list, axis=0)
-        
-    return avg_spec_image
-     
-    
-def show_specs(frame, num_freq_bins, num_time_bins, columns_to_drop):
-	for i in range(len(frame)):
-		print(frame['source_file'].iloc[i])
-		to_plot = frame.drop(columns = columns_to_drop)
-		img = to_plot.iloc[i]
-		img = np.array(img).reshape((num_freq_bins, num_time_bins))
-		plt.imshow(img, origin = 'lower', extent = (num_freq_bins, 0, num_time_bins, 0 ))
-		plt.show()
-
-def files_from_umap(frame, umap1_name, umap2_name, umap1_thresh, umap2_thresh, source_dir):
-
-    #get the spectrograms
-    temp = frame[(frame[umap1_name] > umap1_thresh[0]) & (frame[umap1_name] < umap1_thresh[1]) & (frame[umap2_name] > umap2_thresh[0]) & (frame[umap2_name] < umap2_thresh[1])]
-    source_files = [source_dir+i for i in temp['source_file']]
-    return source_files
-
-def spec_avg_from_umap(frame, umap1_name, umap2_name, umap1_thresh, umap2_thresh, num_freq_bins, num_time_bins):
-
-    #get the spectrograms
-    temp = frame[(frame[umap1_name] > umap1_thresh[0]) & (frame[umap1_name] < umap1_thresh[1]) & (frame[umap2_name] > umap2_thresh[0]) & (frame[umap2_name] < umap2_thresh[1])]
-    
-    if 'label' in temp.columns:
-        avg_spec_raw = np.array(temp.drop(columns = ['label', 'source_file', umap1_name, umap2_name]))
-    else:
-        avg_spec_raw = np.array(temp.drop(columns = ['source_file', umap1_name, umap2_name]))
-    
-    #get the average
-    avg_spec_image = np.mean(avg_spec_raw, axis=0).reshape((num_freq_bins, num_time_bins))
-    
-    #return the individual spectrograms
-    return temp, avg_spec_image
-#TODO - properly comment this. Looks like segmenting_option doesn't do anything
-def wavs_to_umap(segmenting_option, clips_dir, noise_floors_path, species, noise_floor, spec_params, num_to_process, filtered_clips, interpolate, version, save_root):
-#go throught he full pipeline from spectrogram generation to umap plotting
-	
-		
-	#get spectrograms
-	specs_list, source_files = specs_from_wavs(clips_dir = clips_dir, 
-										  noise_floors_path=noise_floors_path, 
-										  species = species,
-										  noise_floor = noise_floor,
-										  spec_params=spec_params, 
-										  num_to_process = num_to_process, 
-										  filtered_clips = filtered_clips, 
-										  interpolate=interpolate)
-										  
-	#linearize
-	specs_lin, shape = linearize_specs(specs_list)
-	del specs_list #free up space
-	print(shape)
-	
-	#zscore
-	df_umap, zscored = zscore_specs(specs_lin, source_files)
-	del specs_lin #free up space
-	
-	#embed
-	umap1, umap2 = get_umap(zscored)
-	del zscored #free up space
-	
-	#plot 
-	print('plotting...')
-	fig = plt.figure(figsize=(10,10))
-	ax = fig.add_subplot(1,1,1)
-	ax = plt.scatter(
-		umap1,
-		umap2,
-		c = 'k',
-		s = 1,
-		alpha = .25, 
-		cmap='viridis')
-	plt.xlim([-20,20])
-	plt.ylim([-20,20])
-	plt.show
-	plots_save_dir = save_root+'umap_plots/'
-	if 'umap_plots' not in os.listdir(save_root):
-		os.mkdir(plots_save_dir)
-	plot_save_name = spec_params['species'] + '_black_' + version + '.jpeg'
-	plt.savefig(plots_save_dir+plot_save_name, dpi=600)
-	
-	#save
-	print('saving umap coordinates...')
-	file_format = '.feather'
-	df_umap = df_umap.reset_index(drop=True)
-	coordinates_save_dir = save_root+'umap_coordinates/'
-	if 'umap_coordinates' not in os.listdir(save_root):
-		os.mkdir(coordinates_save_dir)
-	coordinates_save_name = spec_params['species'] + version + file_format
-	
-	save_name = coordinates_save_dir+coordinates_save_name
-	df_umap.columns = df_umap.columns.map(str)
-	df_umap['umap1'] = umap1
-	df_umap['umap2'] = umap2
-	df_umap.to_feather(save_name)
-	
-	#TODO: get this to work
-	# if coordinates_save_name not in coordinates_save_dir:
-# 		save_umap(df_umap, umap1, umap2, save_name = save_name, file_format = file_format)
-# 	else:
-# 		print('already processed...')
-	
 
 
 	
