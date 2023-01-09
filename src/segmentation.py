d58985db8c113b9d9f1e8534e7f8bcee31017da4
Nicholas Jourjine <jourjine.nicholas@gmail.com>
2023-01-09 07:46:00 -0500

    import save/load parameter functions

--- src/segmentation.py
+++ src/segmentation.py
@@ -15,7 +15,9 @@ import shutil
 import seaborn as sns 
 from scipy.ndimage.filters import gaussian_filter
 from librosa.feature import rms
+
 from src.spectrogramming import ava_get_spec
+from src.parameters import save_parameters, load_parameters
 
 #definitely keep - documented now on evernote 20230105
 def get_amplitude_segments(audio_dir, save_dir, seg_params, species = None, thresholds_path=None, intersyll_threshold = None, duration_threshold = None, path_list = None):
@@ -874,87 +876,7 @@ def save_das_prediction_params(species, model_ID, iteration, min_len, fill_gap, 
 	save_parameters(params_dict, save_dir_root+'00_params/', params_dict['species']+'_'+params_dict['model_ID']+'_das_segmenting_params')
 
 		
-def save_parameters(params, save_dir, save_name):
-    """
-    NOTE this is a copy of the function in utilities.py
-    TODO make your .py files into packages so you can import them instead of copying things like this
-    
-    save a dictionary as .json and double check if you want to overwrite it.
 
-    Parameters
-    ----------
-    params_dict (dict): the parametes dictionary to be saved
-
-    save_dir (str): the path to the place where the dictionary file will be saved
-
-    save_name (str): the name of the file without any file extension
-
-    Returns
-    -------
-    None
-
-    """
-
-    save_path = os.path.join(save_dir,save_name)
-
-    if save_name not in [i.split('.')[0] for i in os.listdir(save_dir)]:
-        print('making a new params file...')
-        with open(save_path+'.json', 'w') as fp:
-            json.dump(params, fp, indent=4)
-        print('saved the params file to:\n',save_path)
-        return
-
-    else: 
-        print('This file already exists in save_dir:', save_name)
-        val = input('overwrite? y/n')
-
-        if val == 'y':
-            val = input('are you sure? y/n')
-
-            if val == 'y':
-                with open(save_path+'.json', 'w') as fp:
-                    json.dump(params, fp, indent=4)
-                print('ok - replaced existing file')
-                return
-
-            elif val == 'n':
-                print('ok - no file saved')
-                return
-
-        elif val == 'n':
-                print('ok - no file saved')
-                return
-
-        else:
-                print('no file saved...')
-                return
-
-    return
-	
-	
-def load_parameters(save_dir, save_name):
-	"""
-	load a dictionary from .json 
-
-	Parameters
-	----------
-	
-	save_dir (str): the path to the place where dictionary was saved
-
-	save_name (str): the name of the .json file (including file extension)
-	
-	Returns
-	-------
-	params_dict (dict): the params dictionary you saved
-
-	"""
-	
-	save_path = os.path.join(save_dir,save_name)
-    
-	with open(save_path+'.json', 'r') as fp:
-			params_dict = json.load(fp)
-	print('loaded parameters from:\n\t', save_path)
-	return params_dict
 def get_silence(pup, audio_dir, seg_dir, save_dir, margin):
     """
     get clip of nonvocal background from each recording to use as a noise floor
